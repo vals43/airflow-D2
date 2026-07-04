56413e10779f419f692de42c3f02720620d9bb90
Teddy Andria <Vals43@users.noreply.huggingface.co>
2026-07-04 12:03:35 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,26 +1,9 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
 
-echo "=== DÉMARRAGE DU SCRIPT D'ENTRÉE ==="
+# Attendre que les configurations système soient prêtes si nécessaire
+clignote_drapeau=0
 
-# Migration de la base Neon propre
-echo "Initialisation et migration de la base Neon..."
-airflow db migrate
+echo "=== DÉMARRAGE D'AIRFLOW EN MODE STANDALONE ==="
 
-# Création de l'utilisateur Admin sécurisée
-echo "Création de l'utilisateur admin..."
-airflow users create \
-    --username admin \
-    --firstname Teddy \
-    --lastname Admin \
-    --role Admin \
-    --email admin@example.com \
-    --password adminpassword || echo "L'utilisateur existe déjà."
-
-# Lancement du scheduler en arrière-plan
-echo "Démarrage du Scheduler..."
-airflow scheduler &
-
-# Lancement du webserver au premier plan
-echo "Démarrage du Webserver sur le port 7860..."
-exec airflow webserver
+# exec permet à Airflow de récupérer le PID 1 et de gérer proprement le cycle de vie du conteneur
+exec airflow standalone
